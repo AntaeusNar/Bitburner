9acc88454774a2be0175435b9487a1d40a897651
AntaeusNar <Jedidiah.h.wallace@gmail.com>
2022-08-13 14:46:55 -0700

    added sleep to loop

--- command_control.js
+++ command_control.js
@@ -318,7 +318,7 @@ class Server {
     * @param {number} [reserveThreads=0] - number of reserved threads
     * @param {number} [indexOfTarget=0] - the current target
     */
-  static adjustTake(targets, maxThreads, numBatchesPerCycle = 0, reserveThreads = 0, indexOfTarget = 0){
+  async static adjustTake(targets, maxThreads, numBatchesPerCycle = 0, reserveThreads = 0, indexOfTarget = 0){
     //Basic function setup
     let ns = targets[0].ns;
     let i = indexOfTarget;
@@ -339,6 +339,7 @@ class Server {
         targets[i].ratioCalc();
         tempVectorsPerCycle = numBatchesPerCycle*targets[i].estVectorsPerBatch;
         reserveThreads += tempVectorsPerCycle;
+        await ns.sleep(1);
     }
 
     //back ratio down if ratio has gone over
