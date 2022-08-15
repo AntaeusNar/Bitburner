89cdca5f4b5e642237256cf005a02e355c4c4ac1
AntaeusNar <Jedidiah.h.wallace@gmail.com>
2022-08-15 16:57:07 -0700

    updated TargetServer class with adjustTake

--- Classes.js
+++ Classes.js
@@ -92,7 +92,7 @@ export class TargetServer extends BasicServer {
   }
 
   //Best case % of server a single hack() thread can take as a decimal to 6 digits
-  get PercentPerSingleHack() {
+  get percentPerSingleHack() {
     if (this.requiredHackingSkill > this.ns.getHackingLevel()) {return null;} //if you call this, and your hack isn't high enough, you get nothing
     let result = null;
     if (can(this.ns, 'Formulas.exe')) {
@@ -134,6 +134,10 @@ export class TargetServer extends BasicServer {
     this.#_takePercent = Math.max(this.#_takePercent, this.percentPerSingleHack);
   }
 
+  //Generates the number of vectors in a single batch
+  get vectorsPerBatch() {
+    return evalVectors(this.ns, this).totalVectors;
+  }
   //Ratio calculation -> thoughts
   //The ratio should always be best possible
   //The ratio should be in $ per Thread per Sec
@@ -144,9 +148,63 @@ export class TargetServer extends BasicServer {
   ratio(batchTime = this.batchTime) {
     if (this.requiredHackingSkill > this.ns.getHackingLevel()) {return null;} //if you call this, and your hack isn't high enough, you get nothing
     let targetTake = this.moneyMax*this.takePercent;
-    let vectorsPerBatch = evalVectors(this.ns, this).totalVectors;
-    return Math.floor(targetTake/vectorsPerBatch/batchTime/1000); // $/thread/Sec
+    return Math.floor(targetTake/this.vectorsPerBatch/batchTime/1000); // $/thread/Sec
   }
+
+
+
+  /** This static function will adjust the takePercent of an array of Server class objects
+    * until array[0].ratio ~= array[1].ratio recusivly.
+    * Because increasing the takePercent is a non-linar increase in the number of needed Threads
+    * this function will loop the increase
+    * In order to prevent a.ratio < b.ratio this will need to stepdown the take after increasing
+    * this should prevent overshooting and messingup prioritization.
+    * This function will also keep the adjustments under the maxThread limit
+    * This will prevent over commitment vs a single target
+    * Additionally, the number of vectors per cycle will be caclulated per
+    * the number of batches per cycle of the primary target.
+    * This ensures that if b can be hit faster then a (lower weakentime)
+    * there are not unusable threads reserved as used.
+    * @param {NS} ns
+    * @param {array} targets - array of Server class objects
+    * @param {number} maxThreads
+    * @param {number} [numBatchesPerCycle=0] - the control speed
+    * @param {number} [reserveThreads=0] - number of reserved threads
+    * @param {number} [indexOfTarget=0] - the current target
+    */
+  static async adjustTake(ns, targets, maxThreads, numBatchesPerCycle = 0, reserveThreads = 0, indexOfTarget = 0) {
+    let i = indexOfTarget;
+    let tempVectorsPerCycle = 0
+    if (i == 0) {
+      numBatchesPerCycle = targets[0].batchesPerCycle;
+      reserveThreads = numBatchesPerCycle*targets[0].vectorsPerBatch;
+    }
+
+    //Run a loop to increase the take, and the ratio while
+    //there are threads available, a next target, ratio is greater the the Next
+    //and the take is less then 99%
+  while (reserveThreads < maxThreads &&
+    targets[i].ratio() > targets[i+1].ratio(targets[i].batchTime) &&
+    targets[i].takePercent < .999) {
+      let oldThreads = numBatchesPerCycle*targets[i].vectorsPerBatch;
+      let takeIncrease = Math.max(this.percentPerSingleHack, .001);
+      targets[i].takePercent = Math.round((targets[i].takePercent + takeIncrease)*1000)/1000;
+      let newThreads = numBatchesPerCycle*targets[i].vectorsPerBatch;
+      reserveThreads = reserveThreads + (newThreads - oldThreads);
+      await ns.sleep(1);
+    }
+
+    if (reserveThreads >= maxThreads) {
+      logger(ns, 'INFO: max threads hit, stopping take increase calc.');
+    } else if (indexOfTarget >= targets.length) {
+      logger(ns, 'INFO: hit last available target, stopping take increase clac.');
+    } else {
+      logger(ns, 'INFO: Calcuclating take for next target.');
+      indexOfTarget++;
+      await TargetServer.adjustTake(ns, targets, maxThreads, numBatchesPerCycle, reserveThreads, indexOfTarget);
+    }
+  }//end of static adjustTake
+
 }//end of Target Server class
 
 /** Server Factory when given a hostname, will make a server object that is either basic, drone, or target */
