816ce4f50d3d780d972d799e1d49eebed69f8ced
AntaeusNar <Jedidiah.h.wallace@gmail.com>
2022-08-18 08:22:35 -0700

    fised comment

--- options.js
+++ options.js
@@ -1,3 +1,3 @@
-/** Various runtime options for command_control.js
+/** Various runtime options for command_control.js */
 
 export const baseDelay = 100; //base delay between batches and cycles in milliseconds
