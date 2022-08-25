d008552dac7ed510c0cea5c7905ce9c8dfe560ff
AntaeusNar <Jedidiah.h.wallace@gmail.com>
2022-08-25 15:00:31 -0700

    setting adjustment to help limit thread dunching...not perfect

--- options.js
+++ options.js
@@ -1,4 +1,4 @@
 /** Various runtime options for command_control.js */
 
-export const baseDelay = 100; //base delay between batches and cycles in milliseconds
+export const baseDelay = 200; //base delay between batches and cycles in milliseconds // NOTE: decreasing below 200 causes bunching of scripts
 export const maxScripts = 10000; //max number of deployable scripts.  > 10,000 seems to create realworld game crashes
