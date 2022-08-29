a5cde85503b901aacfb1d4839f13956bc00cbb50
AntaeusNar <Jedidiah.h.wallace@gmail.com>
2022-08-29 08:22:24 -0700

    added available budget multiplier

--- options.js
+++ options.js
@@ -2,3 +2,4 @@
 
 export const baseDelay = 200; //base delay between batches and cycles in milliseconds // NOTE: decreasing below 200 causes bunching of scripts
 export const maxScripts = 10000; //max number of deployable scripts.  > 10,000 seems to create realworld game crashes
+export const budget = .5; // multipler for available budget.
