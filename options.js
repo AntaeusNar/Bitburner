83606f676b175d400c1848eec2dbed48d6d5a84f
AntaeusNar <Jedidiah.h.wallace@gmail.com>
2022-08-29 08:23:41 -0700

    added paybackTime

--- options.js
+++ options.js
@@ -3,3 +3,4 @@
 export const baseDelay = 200; //base delay between batches and cycles in milliseconds // NOTE: decreasing below 200 causes bunching of scripts
 export const maxScripts = 10000; //max number of deployable scripts.  > 10,000 seems to create realworld game crashes
 export const budget = .5; // multipler for available budget.
+export const paybackTime = 1 * 60 * 60; //Time in seconds for payback
