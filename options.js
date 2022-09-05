d502e728e916b8b5f1aecd346f6f6debf3a77c7b
AntaeusNar <Jedidiah.h.wallace@gmail.com>
2022-09-05 10:40:16 -0700

    added bitnodemultis to options

--- options.js
+++ options.js
@@ -4,3 +4,8 @@ export const baseDelay = 50; //base delay between batches and cycles in millisec
 export const maxScripts = 10000; //max number of deployable scripts.  > 10,000 seems to create realworld game crashes
 export const budgetPercentageLimit = .5; // multipler for available budget.
 export const paybackTimeLimit = 1 * 60 * 60; //Time in seconds for payback
+export const bitNodeMultipliers = { //bitNodeMultipliers
+  ServerGrowthRate: 1/Math.pow(1.02, 6),
+  ScriptHackMoney: 1/Math.pow(1.02, 6),
+  HackNetNodeMoneyBitNode: 1/Math.pow(1.02, 6);
+}
