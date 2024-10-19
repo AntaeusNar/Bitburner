aace9e389ceb4ea3084a46cd06584290aaebe3c6
AntaeusNar <jedidiah.h.wallace@gmail.com>
2024-10-19 14:19:39 -0700

    Logger as a class

--- lib.misc.js
+++ lib.misc.js
@@ -1,22 +1,62 @@
-/** Library of general functions */
+/** Library of general functions and useful classes */
 
+/** Logger Singleton class */
+export class Logger {
 
-/** Logger print function
- * @param {NS} ns
- * @param {string} message
- * @param {number} [options=2] - 0 program log, 1 terminal log, 2 both
- */
-export function logger(ns, message, options=2) {
-    switch (options) {
-      case 0:
-        ns.print(message);
-        break;
-      case 1:
-        ns.tprint(message);
-        break;
-      case 2:
-      default:
-        ns.print(message);
-        ns.tprint(message);
+    /**
+     * Constructor
+     * @param {NS} ns 
+     * @param {number} logLevel level of log 0-7 {@link https://en.wikipedia.org/wiki/Syslog}
+     */
+    constructor(ns, logLevel) {
+        this.ns = ns;
+        this.logLevel = logLevel;
     }
-   }
+
+    emerg(message) {
+        this.ns.print(message);
+        this.ns.tprint(message);
+    }
+
+    alert(message) {
+        if (this.logLevel <= 1 ) { return }
+        this.ns.print(message);
+        this.ns.tprint(message);
+    }
+
+    crit(message) {
+        if (this.logLevel <= 2 ) { return }
+        this.ns.print(message);
+        this.ns.tprint(message);
+    }
+
+    err(message) {
+        if (this.logLevel <= 3 ) { return }
+        this.ns.print(message);
+        this.ns.tprint(message);
+    }
+
+    warning(message) {
+        if (this.logLevel <= 4 ) { return }
+        this.ns.print(message);
+        this.ns.tprint(message);
+    }
+
+    notice(message) {
+        if (this.logLevel <= 5 ) { return }
+        this.ns.print(message);
+        this.ns.tprint(message);
+    }
+
+    info(message) {
+        if (this.logLevel <= 6 ) { return }
+        this.ns.print(message);
+        this.ns.tprint(message);
+    }
+
+    debug(message) {
+        if (this.logLevel <= 7 ) { return }
+        this.ns.print(message);
+        this.ns.tprint(message);
+    }
+}
