62320124abd10a8b3d82a0d3ca56936dd442f042
AntaeusNar <jedidiah.h.wallace@gmail.com>
2024-10-20 14:04:10 -0700

    Updated Options

--- home/etc/options.js
+++ home/etc/options.js
@@ -2,14 +2,14 @@
 
 export const baseDelay = 50; //base delay between batches and cycles in milliseconds // NOTE: decreasing below 200 causes bunching of scripts
 export const maxScripts = 10000; //max number of deployable scripts.  > 10,000 seems to create realworld game crashes
-export const budgetPercentageLimit = .5; // multipler for available budget.
+export const budgetPercentageLimit = .5; // multiplier for available budget.
 export const paybackTimeLimit = 1 * 60 * 60; //Time in seconds for payback
 export const bitNodeMultipliers = { //bitNodeMultipliers ->placeholder for bitnode info -> https://github.com/danielyxie/bitburner/blob/dev/src/BitNode/BitNode.tsx
   ServerGrowthRate: 1/Math.pow(1.02, 6),
   ScriptHackMoney: 1/Math.pow(1.02, 6),
   HackNetNodeMoneyBitNode: 1/Math.pow(1.02, 6),
 };
-export const serverOfIntrest = ['CSEC', 'avmnite-02h', 'I.I.I.I', 'run4theh111z', 'w0r1d_d43m0n'];
+export const serverOfInterest = ['CSEC', 'avmnite-02h', 'I.I.I.I', 'run4theh111z', 'w0r1d_d43m0n'];
 export const colors = {
 		black: "\u001b[30m",
 		red: "\u001b[31m",
