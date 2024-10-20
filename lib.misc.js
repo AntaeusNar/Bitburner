41653b35d48bd0f7030e3b552c9d30cde618b087
AntaeusNar <jedidiah.h.wallace@gmail.com>
2024-10-19 17:58:55 -0700

    Added can

--- lib.misc.js
+++ lib.misc.js
@@ -102,3 +102,13 @@ export function recServerScan(ns, serverName='home') {
     scanning(serverName);
     return serverList;
 }
+
+/** Checks for existence of file on the specified or home server
+  * @param {NS} ns
+  * @param {string} file - File Name
+  * @param {string} [serverName=home] - hostname of server
+  * @return {boolean} true if file exists on the server
+  */
+export function can(ns, file, serverName='home') {
+	return ns.fileExists(file, serverName);
+}
