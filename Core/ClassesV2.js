bb41f41fca0fccbbf9a892aaae0b4dc9c7699b53
AntaeusNar <jedidiah.h.wallace@gmail.com>
2025-02-03 15:09:01 -0800

    fixed home not being  counted in maxram

--- Core/ClassesV2.js
+++ Core/ClassesV2.js
@@ -206,7 +206,7 @@ export class ServerFactory {
       }
 
       if (evalServer.maxRam > 0) {
-        if (evalServer.root) {
+        if (evalServer.root || evalServer.hostname == 'home') {
           inventory.drones.push(evalServer);
         } else {
           inventory.inactiveDrones.push(evalServer);
