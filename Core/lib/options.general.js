32ea3e644332b13063df0a0a4455d5bf33664f48
AntaeusNar <jedidiah.h.wallace@gmail.com>
2025-02-04 19:50:44 -0800

    added general options

---
+++ Core/lib/options.general.js
@@ -0,0 +1,5 @@
+export const baseDelay = 50; //base delay between batches and cycles in milliseconds // NOTE: decreasing below 200 causes bunching of scripts
+export const currentBitNode = {
+	n: 12,
+	lvl: 1
+};
