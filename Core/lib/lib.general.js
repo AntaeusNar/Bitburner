8e724e189d34eadb0803b07a468fb4263956234c
AntaeusNar <jedidiah.h.wallace@gmail.com>
2025-02-04 19:51:03 -0800

    calc times

--- Core/lib/lib.general.js
+++ Core/lib/lib.general.js
@@ -1,3 +1,10 @@
+import { MyServer } from "./class.MyServer";
+import { localBitNodeMultipliers } from "./options.bitNode";
+import { currentBitNode } from "./options.general";
+
+
+const BitMults = localBitNodeMultipliers(currentBitNode.n, currentBitNode.lvl);
+
 /** Checks for existence of file on the specified or home server
   * @param {NS} ns
   * @param {string} file - File Name
@@ -49,4 +56,68 @@ export function multiScan(ns, serverName='home') {
   }
   scanning(serverName);
   return serverList;
+}
+
+/**
+ * Returns time it takes to complete a grow on a server, in seconds
+ * -> https://github.com/bitburner-official/bitburner-src/blob/dev/src/Hacking.ts#80
+ * @param {MyServer} server
+ * @param {Object} player
+ * @param {boolean} [planning = false]
+ * @returns {number} seconds
+ */
+export function calculateGrowTime(server, player, planning = false) {
+    const growthTimeMultiplier = 3.2;
+    return growthTimeMultiplier * calculateHackingTime(server, player, planning);
+}
+
+/**
+ * Returns time it takes to complete a weaken on a server, in seconds
+ * -> https://github.com/bitburner-official/bitburner-src/blob/dev/src/Hacking.ts#L87
+ * @param {MyServer} server
+ * @param {Object} player
+ * @param {boolean} [planning = false]
+ * @returns {number} seconds
+ */
+export function calculateWeakenTime(server, player, planning = false) {
+    const weakenTimeMultiplier = 4;
+    return weakenTimeMultiplier * calculateHackingTime(server, player, planning)
+}
+
+/**
+ * Returns time it takes to complete a hack on a server, in seconds
+ * -> https://github.com/bitburner-official/bitburner-src/blob/dev/src/Hacking.ts#L58
+ * @param {MyServer} server
+ * @param {Object} player
+ * @param {boolean} [planning = false]
+ * @returns {number} seconds
+ */
+export function calculateHackingTime(server, player, planning = false) {
+    const serverSecurity = planning ? server.minDifficulty : server.currentDifficulty;
+    const difficultyMult = server.requiredHackingSkill * serverSecurity;
+
+    const baseDiff = 500;
+    const baseSkill = 50;
+    const diffFactor = 2.5;
+    let skillFactor = diffFactor * difficultyMult + baseDiff;
+    skillFactor /= player.skills.hacking + baseSkill;
+
+    const hackTimeMultiplier = 5;
+    const hackingTime =
+      (hackTimeMultiplier * skillFactor) /
+      (player.mults.hacking_speed *
+        BitMults.HackingSpeedMultiplier *
+        calculateIntelligenceBonus(player.skills.intelligence, 1));
+
+    return hackingTime;
+}
+
+/**calculate Intelligence bonus
+ * ->https://github.com/danielyxie/bitburner/blob/dev/src/PersonObjects/formulas/intelligence.ts
+ * @param {number} intelligence
+ * @param {number} weight
+ * @return {number}
+ */
+export function calculateIntelligenceBonus(intelligence, weight) {
+    return 1 + (weight * Math.pow(intelligence, 0.8))/600;
 }
