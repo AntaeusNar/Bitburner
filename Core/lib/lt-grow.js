4f0e22c3ad4326904dec219cbaebd9510e5a90e1
AntaeusNar <jedidiah.h.wallace@gmail.com>
2025-02-06 15:36:39 -0800

    updated sleep time issues to handle sleep time in secs vs miliseconds

--- Core/lib/lt-grow.js
+++ Core/lib/lt-grow.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
 let target = ns.args[0];
-	let sleeptime = ns.args[1];
+	let sleeptime = ns.args[1] * 1000;
 	await ns.sleep(sleeptime);
 	await ns.grow(target);
 }
